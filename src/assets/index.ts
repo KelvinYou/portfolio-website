d4c161ee72175beb48808cc487416235e2ce76a4
Kelvin <ykekelvin0220@gmail.com>
2023-07-18 08:05:29 +0800

    first commit

---
+++ src/assets/index.ts
@@ -0,0 +1,2 @@
+export { default as techtics } from "./company/techtics.png";
+export { default as finexus } from "./company/finexus.png";
---
+++ src/constants/data.ts
@@ -0,0 +1,62 @@
+import {
+  finexus,
+  techtics
+} from "@/assets";
+
+const experiences = [
+  {
+    title: "Java Software Engineer Intern",
+    company_name: "Finexus International Sdn Bhd",
+    icon: finexus,
+    iconBg: "#383E56",
+    date: "Feb 2023 - Jul 2023",
+    points: [
+      "Focused on Java-based project maintenance, utilizing JSP and Tomcat. Conducted R&D for innovative features such as reusable custom dialogs and remote log file retrieval. Conducted comprehensive testing and documentation for quality assurance.",
+      "Contributed to large-scale projects involving multiple teams, including frontend, backend, and mobile, fostering effective collaboration and coordination for successful project execution.",
+      "Skilled in utilizing log files for debugging, adept at efficient web resource searching, and knowledgeable in SIT and UAT workflows.",
+      "Proficient in deploying applications using Tomcat, SQLDeveloper, and FileZilla for streamlined server management and file transfer."
+      // "Developing and maintaining web applications using React.js and other related technologies.",
+      // "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      // "Implementing responsive design and ensuring cross-browser compatibility.",
+      // "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+    skills: [
+      "React.js",
+      "Java for Android",
+      "Firebase",
+      "Oracle SQL",
+      "XML",
+      "JavaServer Pages (JSP)",
+      "Linux",
+      "Jasper",
+      "TortoiseSVN"
+    ],
+  },
+  {
+    title: "Web Developer Intern",
+    company_name: "Techtics Blockchain PLT",
+    icon: techtics,
+    iconBg: "#E6DEDD",
+    date: "Oct 2020 - Jan 2021",
+    points: [
+      "Revitalized project's frontend using ReactJS for enhanced maintenance, modernization, and dynamic website capabilities.",
+      "Developed a cutting-edge blockchain application utilizing ReactJS and Solidity to seamlessly integrate smart contracts with MetaMask for secure and streamlined transactions.",
+      "Explored Laravel's router functionality, MVC architecture, ORM, events, listeners, and data seeding techniques for efficient database testing.",
+      "Led a Laravel project under senior guidance, focusing primarily on frontend development. Implemented key functionalities such as authentication, real-time exchange rate fetching, dynamic UI updates, and built APIs for seamless CRUD operations on transactions.",
+      
+      // "Developing and maintaining web applications using React.js and other related technologies.",
+      // "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
+      // "Implementing responsive design and ensuring cross-browser compatibility.",
+      // "Participating in code reviews and providing constructive feedback to other developers.",
+    ],
+    skills: [
+      "Laravel PHP",
+      "Typescript",
+      "Javascript",
+      "ReactJS",
+      "ExpressJS",
+      "PuppeteerJS",
+      "Git"
+    ],
+  }
+];
