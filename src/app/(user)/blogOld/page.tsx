860e7f253df4d3e4d5971df65e8084d1fd16faf0
Kelvin <ykekelvin0220@gmail.com>
2024-05-20 01:48:29 +0800

    feat(blog): revamp with mdx 15%

---
+++ src/app/(user)/blogOld/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import BlogHome from './BlogHome'
+import SpotlightPage from '../_components/SpotlightPage'
+
+const BlogPage = () => {
+  return (
+    <>
+      <BlogHome />
+
+      {/* <SpotlightPage /> */}
+    </>
+  )
+}
+
+export default BlogPage
