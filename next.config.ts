827cf8a87c570fd6ebe49672434862d7433a3860
Kelvin You <ykekelvin0220@gmail.com>
2025-03-05 16:21:56 +0800

    bugfix: debug 2

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  transpilePackages: ['crypto-js'],
+  // Add any crypto-using routes to this list if needed
+  experimental: {
+    optimizeCss: true,
+  }
 };
 
 export default nextConfig;
