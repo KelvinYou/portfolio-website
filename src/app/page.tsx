1b29a7d721fb0634a52d4922db63a3c7dcbc9817
Kelvin You <ykekelvin0220@gmail.com>
2025-03-06 09:18:00 +0800

    fix: add skills and contact

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { AboutSection } from "@/components/sections/about-section";
 import { EducationsSection } from "@/components/sections/educations-section";
 // import { ExperiencesSection } from "@/components/sections/experiences-section";
 import { ProjectsSection } from "@/components/sections/projects-section";
-// import { SkillsSection } from "@/components/sections/skills-section";
-// import { ContactSection } from "@/components/sections/contact-section";
+import { SkillsSection } from "@/components/sections/skills-section";
+import { ContactSection } from "@/components/sections/contact-section";
 
 export default function Home() {
   return (
@@ -14,8 +14,8 @@ export default function Home() {
       <EducationsSection />
       {/* <ExperiencesSection /> */}
       <ProjectsSection />
-      {/* <SkillsSection />
-      <ContactSection /> */}
+      <SkillsSection />
+      <ContactSection />
     </div>
   );
 }
